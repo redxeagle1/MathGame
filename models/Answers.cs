8fc7e3cc1e190a070e239e77f7925eacc0dbeec9
Saad Salama <redxeagle97@gmail.com>
2026-09-20 23:09:36 +0300

    feat: finished implementing Answers.cs and tested it in playground.verso

---
+++ models/Answers.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Runtime.InteropServices;
+namespace MathGame;
+
+
+public struct Answers : IEnumerable
+// an struct for The MCQ question type
+{
+    #region Fields
+    // A list that contains our answers
+    private readonly List<int> _answerList = new(4);
+    #endregion
+    #region Properties
+
+    // indexer to iterate through the answers
+    public readonly int this[int index] => _answerList[index];
+    public int AnswerIndex {get;set;}
+    #endregion
+    #region Constructor
+    public Answers(int answer)
+    {
+        // add the main answer
+        _answerList.Add(answer);
+
+        // add the rest of the answers
+        GenerateAnswerCandidates(answer);
+
+        // shuffle the elements in place
+        Random.Shared.Shuffle(CollectionsMarshal.AsSpan(_answerList));        
+        AnswerIndex = _answerList.IndexOf(answer);
+    }
+    #endregion
+    #region Methods
+    
+    private readonly void GenerateAnswerCandidates(int a)
+    // this method generate the keys and there values in the dictionary
+    {
+        // iterating through the given counter
+        for (int i = 1; i < 4; i++)
+        {
+            // getting a temp value from a the non-negative range of possibilities 
+            int temp = Random.Shared.Next(a/2,a*2);
+            
+            // if the generated integer happens to be already exist enter a loop of generation
+            // till getting a value
+            while (_answerList.Contains(temp))
+            {
+                temp = Random.Shared.Next(a/2,a*2);
+            }    
+
+            // after loop
+            _answerList.Add(temp);
+        }
+    }
+
+    // Important to use String.Join on it
+    public readonly IEnumerator<int> GetEnumerator() => _answerList.GetEnumerator();
+    readonly IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    #endregion
+}
