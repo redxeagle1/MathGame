54fc5f7cb93af3d816fa4b7cb245f52c2f4aa406
Saad Salama <redxeagle97@gmail.com>
2026-09-21 16:30:23 +0300

    feat: finished implementing AnswerTF.cs and tested it in playground.verso

---
+++ AnswerTF.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Runtime.InteropServices;
+namespace MathGame;
+
+public struct AnswerTF 
+
+{
+    #region Fields
+    #endregion
+    #region Properties
+    public int WrongAnswer{get;}
+    public int CorrectAnswer{get;}
+    #endregion
+    #region Constructor
+    public AnswerTF(int answer)
+    {
+        CorrectAnswer = answer;
+        int temp = Random.Shared.Next(answer/2,answer*2);
+        WrongAnswer = ( temp == answer ) ? Random.Shared.Next(answer/2,answer) : temp;
+    }
+    #endregion
+    #region Methods
+
+    #endregion
+
+}
+// an struct for The MCQ question type
